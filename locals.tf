87b211215f8c7560cd5661559bf452edca58998d
Chip Bell <cbell@pbs.org>
2025-11-04 09:42:31 -0500

    Fixing a logic bug
    If create_lb was false, this module would still create the load balancer
    because the equality check was against null. This makes that logic match
    expectations a little better

--- locals.tf
+++ locals.tf
@@ -32,7 +32,7 @@ locals {
   nlb_eips                             = local.create_nlb && var.create_attach_eip_to_nlb == true ? local.subnets : []
   http_application_rule_count          = local.only_create_http_listener ? length(local.aliases) : 0
   https_application_rule_count         = local.create_https_listeners ? length(local.aliases) : 0
-  create_lb                            = var.create_lb != null
+  create_lb                            = var.create_lb == true
   create_cidr_access_rule              = length(var.restricted_cidr_blocks) > 0
   create_sg_access_rule                = var.restricted_sg != null
   create_nlb_cidr_access_rule          = local.create_nlb && local.create_cidr_access_rule
